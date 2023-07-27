69e128f5d821072f6f100da7e19a938d79d8874f
mtakahashi1011 <mtakahashi1011@gmail.com>
2023-07-27 13:20:39 +0900

    create _dot_var function

--- MAIN_TEST.py
+++ MAIN_TEST.py
@@ -3,11 +3,19 @@ import numpy as np
 
 # OK
 # from dezero.core_simple import Variable
-from dezero import Variable 
+#from dezero import Variable 
+from dezero import *
+from dezero.utils import _dot_var
 
 class Dezero_Test(unittest.TestCase):
     def test_package(self):
         x = Variable(np.array(1.0))
         self.assertEqual(x.data, np.array(1.0))
+
+    def test_dot_var(self):
+        x = Variable(np.random.randn(2, 3))
+        x.name = 'x'
+        print(_dot_var(x))
+        print(_dot_var(x, verbose=True))
 
 unittest.main()
