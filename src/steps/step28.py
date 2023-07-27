607f6fe4d34fbbf82ab9b5d2e9ff3a6cca4fb8f2
mtakahashi1011 <mtakahashi1011@gmail.com>
2023-07-27 16:57:28 +0900

    test gradient descent method

---
+++ src/steps/step28.py
@@ -0,0 +1,29 @@
+import numpy as np 
+import sys
+import os 
+
+path = os.path.join(os.path.dirname(__file__), '../../')
+sys.path.append(path)
+
+from dezero import *
+from dezero.utils import *
+
+def rosenbrock(x0, x1):
+    y = 100 * (x1 - x0**2)**2 + (x0 - 1)**2
+    return y
+
+x0 = Variable(np.array(0.0))
+x1 = Variable(np.array(2.0))
+
+lr = 0.001
+iters = 10000
+
+for i in range(iters):
+    print(x0.data, x1.data)
+
+    y = rosenbrock(x0, x1)
+    x0.cleargrad()
+    x1.cleargrad()
+    y.backward()
+    x0.data -= lr * x0.grad 
+    x1.data -= lr * x1.grad 
