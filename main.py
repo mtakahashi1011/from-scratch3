1b3387f55ce30df9f8affdf5c69d78b4a9a97fdc
mtakahashi1011 <133080291+mtakahashi1011@users.noreply.github.com>
2023-07-31 04:13:27 +0000

    create Optimizer class

--- main.py
+++ main.py
@@ -2,16 +2,19 @@ import numpy as np
 from dezero import Variable, Model, MLP
 import dezero.functions as F 
 import dezero.layers as L 
+from dezero import optimizers
 
 np.random.seed(0)
 x = np.random.rand(100, 1)
 y = np.sin(2 * np.pi * x) + np.random.rand(100, 1)
 
-model = MLP((10, 1))
-model.plot(x, to_file='graph_img/two_layer_net2.png')
-
 lr = 0.2
 iters = 10000
+
+model = MLP((10, 1))
+optimizer = optimizers.MomentumSGD(lr)
+optimizer.setup(model)
+model.plot(x, to_file='graph_img/two_layer_net2.png')
 
 for i in range(iters):
     y_pred = model(x)
@@ -20,7 +23,6 @@ for i in range(iters):
     model.cleargrads()
     loss.backward()
 
-    for p in model.params():
-        p.data -= lr * p.grad.data 
+    optimizer.update()
     if i %1000 == 0:
         print('iters: ', i, 'loss: ', loss.data)
